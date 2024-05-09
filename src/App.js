e3cbc5a25c353f7ffad59de1481e9cdff64ff985
Kaleidiana <kaleidiana63@gmail.com>
2024-05-09 12:50:25 +0300

    Upload files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import Sample from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a simple blog page</h1>
+      <Sample /> 
+    </div>
+  );
+}
+
+export default App;
